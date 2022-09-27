35bda84a4d419bdb75536fc78d5fef0996977d8a
Krystian Jarmicki <jarmicki@gmail.com>
2022-09-27 18:09:21 +0200

    item 6 cont.

--- 6-interface-on-the-producer-side.go
+++ 6-interface-on-the-producer-side.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/kjarmicki/100-go-mistakes/interfaces"
 )
 
@@ -10,6 +8,10 @@ import (
  * In Go, iterfaces are satisfied implicitly. This means that the consuming side can define a right
  * level of abstraction and there's no need to expose a best-guess interface from the producer.
  * Some clients may want the whole thing, some may want only one method. Let them decide.
+ *
+ * There are exceptions though: throughout standard library there are interfaces declared in the producer
+ * packages. These interfaces are known to be useful and were discovered (not foreseen) when developing
+ * a standard library. If you know the needs of the clients, you may provide useful interfaces.
  */
 
 type customerGetter interface {
@@ -22,7 +24,7 @@ func InterfaceOnTheProducerSide() {
 		if err != nil {
 			return err
 		}
-		fmt.Println(customer)
+		_ = customer
 		return nil
 	}
 
