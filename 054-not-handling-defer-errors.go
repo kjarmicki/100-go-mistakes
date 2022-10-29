bdb76939cc43ecf95be90a8495d6c8fec51abdb8
Krystian Jarmicki <jarmicki@gmail.com>
2022-10-29 13:21:46 +0200

    chapter 54 ammendment

--- 054-not-handling-defer-errors.go
+++ 054-not-handling-defer-errors.go
@@ -40,8 +40,29 @@ func getBalancePropagate(db *sql.DB, clientId string) (count float32, err error)
 		return 0, err
 	}
 	// alternative - capture err variable and assign it before returning
+	// warning! this has a significant problem of *always* returning the err from rows.Close()
 	defer func() {
 		err = rows.Close()
+	}()
+	return 0, nil
+}
+
+func getBalancePropagateOrLog(db *sql.DB, clientId string) (count float32, err error) {
+	rows, err := db.Query("...", clientId)
+	if err != nil {
+		return 0, err
+	}
+	// most robust
+	defer func() {
+		closeErr := rows.Close()
+		if err != nil {
+			if closeErr != nil { // if both errors happened, log closeErr and propagate regular err
+				log.Printf("failed to close rows: %v", closeErr)
+			}
+			return
+		}
+		// otherwise, propagate closeErr
+		err = closeErr
 	}()
 	return 0, nil
 }
